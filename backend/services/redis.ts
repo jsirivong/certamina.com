9ee8a64319ea77d74e39c04d050ad229bdc283fc
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-26 21:03:57 -0400

    establish a connection to redis database

---
+++ backend/services/redis.ts
@@ -0,0 +1,12 @@
+import Redis from 'ioredis';
+
+const REDIS_URL = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+
+export const redis = new Redis(REDIS_URL);
+
+redis.on("connect", ()=>{
+    console.log("Redis connected.")
+})
+redis.on("error", (err)=>{
+    console.log("Trouble connecting to Redis.\n",err)
+})
