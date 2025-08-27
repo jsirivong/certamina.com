917388c0851874e25186430baa1499e9f56175c4
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-26 21:03:22 -0400

    add room router

---
+++ backend/routes/room.route.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import { createRoom, joinRoom } from '../controllers/room.controller.ts';
+import { authorizeUser } from '../middleware/authorization.ts';
+
+const router = express.Router();
+
+router.post("/", authorizeUser, createRoom);
+router.post("/:code/join", authorizeUser, joinRoom)
+
+export default router;
+
