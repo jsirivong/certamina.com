52430e9770956817b1b5f2ad446d5aa8515b30fd
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-26 21:06:15 -0400

    create a page loader

---
+++ frontend/src/components/PageLoading.tsx
@@ -0,0 +1,7 @@
+export default function PageLoading(){
+    return (
+        <div className="h-screen flex items-center justify-center" data-theme="light">
+            <div className="loading loading-spinner loading-xl"></div>
+        </div>
+    )
+}
