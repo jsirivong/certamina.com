eed9e8844d869de4ad52405ce1cc0c50785a5b58
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-26 21:03:43 -0400

    add password hashing utils

---
+++ backend/services/password.ts
@@ -0,0 +1,12 @@
+import bcrypt from 'bcrypt';
+
+export const hashPassword = async (password: string): Promise<string> => {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    return hashedPassword
+}
+
+export const comparePassword = (password: string, hash: string): boolean => {
+    return bcrypt.compare(password, hash)
+}
