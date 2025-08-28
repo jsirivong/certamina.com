eb0bcaca20e2f3f8f9fed027c56e92ef5f343324
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-28 05:04:34 -0400

    database connection

--- backend/lib/database.ts
+++ backend/lib/database.ts
@@ -1,3 +1,13 @@
 import { neon } from '@netlify/neon';
+import postgres from 'postgres';
+import dotenv from 'dotenv'
 
-export const sql = neon();
+dotenv.config();
+
+export const sql: any = process.env.NODE_ENV === "development" ? postgres({
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT) || 5432,
+    database: process.env.PG_DATABASE,
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD
+}) : neon();
