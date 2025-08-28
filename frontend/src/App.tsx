7f1722adeb326c5fe4597a9d2fce5781ece0c80c
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-28 03:19:33 -0400

    implemented routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,40 +5,67 @@ import Login from "./pages/Login";
 import Footer from "./components/Footer.tsx";
 import { useEffect } from "react";
 import PageLoading from "./components/PageLoading.tsx";
-import useAuthentication from "./hooks/useAuthentication.tsx";
 import Join from "./pages/Join.tsx";
 import Room from "./pages/Room.tsx";
 import Navbar from "./components/Navbar.tsx";
 import About from "./pages/About.tsx";
 import Practice from "./pages/Practice.tsx";
 import Contact from "./pages/Contact.tsx";
+import axios from "./services/axios.ts";
+import { useState } from "react";
 
-export default function App(){
-  const { loading, user, checkAuthentication } = useAuthentication();
+export default function App() {
+  const [user, setUser] = useState<{ email: string, username: string } | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    checkAuthentication();
+    (async () => {
+      try {
+        const response = await axios.get("/auth")
+
+        if (response.data.success) {
+          setUser(response.data.user)
+        } else {
+          setUser(null)
+        }
+      } catch (err: any) {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    })()
   }, [])
 
-  const isAuthenticated = Boolean(user);
+  const handleLogout = async () => {
+    try {
+      const response = await axios.post("/auth/logout");
+
+      if (response.data.success) {
+        console.log("Successfully logged out");
+        setUser(null);
+      }
+    } catch (err: any) {
+      console.error(err);
+    }
+  }
 
   if (loading) {
     return (
-      <PageLoading/>
+      <PageLoading />
     )
   }
 
   return (
     <div data-theme="light">
       <Routes>
-        <Route path="/" element={<Footer><Navbar><Home/></Navbar></Footer>}/>
-        <Route path="/register" element={!isAuthenticated ? (<Footer><Register/></Footer>) : <Navigate to={"/"}/>}/>
-        <Route path="/login" element={!isAuthenticated ? <Footer><Login/></Footer> : <Navigate to={"/"}/>} />
-        <Route path="/join" element={<Navbar><Join/></Navbar>}/>
-        <Route path="/room/:code" element={<Navbar><Room/></Navbar>}/>
-        <Route path="/about" element={<Navbar><Footer><About/></Footer></Navbar>}/>
-        <Route path="/practice" element={<Navbar><Practice/></Navbar>}/>
-        <Route path="/contact" element={<Navbar><Footer><Contact/></Footer></Navbar>}/>
+        <Route path="/" element={<Footer><Navbar username={user?.username} isAuthenticated={Boolean(user)} handleLogout={handleLogout}><Home /></Navbar></Footer>} />
+        <Route path="/register" element={!Boolean(user) ? (<Footer><Register /></Footer>) : <Navigate to={"/"} />} />
+        <Route path="/login" element={!Boolean(user) ? <Footer><Login /></Footer> : <Navigate to={"/"} />} />
+        <Route path="/join" element={<Navbar username={user?.username} isAuthenticated={Boolean(user)} handleLogout={handleLogout}><Join /></Navbar>} />
+        <Route path="/room/:code" element={<Navbar username={user?.username} isAuthenticated={Boolean(user)} handleLogout={handleLogout}><Room /></Navbar>} />
+        <Route path="/about" element={<Navbar username={user?.username} isAuthenticated={Boolean(user)} handleLogout={handleLogout}><Footer><About /></Footer></Navbar>} />
+        <Route path="/practice" element={<Navbar username={user?.username} isAuthenticated={Boolean(user)} handleLogout={handleLogout}><Practice /></Navbar>} />
+        <Route path="/contact" element={<Navbar username={user?.username} isAuthenticated={Boolean(user)} handleLogout={handleLogout}><Footer><Contact /></Footer></Navbar>} />
       </Routes>
     </div>
   )
