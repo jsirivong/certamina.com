5a00880926cca0f7602cd2a3092e242fdc9f1623
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-28 14:22:58 -0400

    database config

--- backend/lib/database.ts
+++ backend/lib/database.ts
@@ -4,10 +4,15 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-export const sql: any = process.env.NODE_ENV === "development" ? postgres({
+export let sql;
+
+if (process.env.NODE_ENV === "development"){
+    sql = postgres({
     host: process.env.PG_HOST,
     port: Number(process.env.PG_PORT) || 5432,
     database: process.env.PG_DATABASE,
     username: process.env.PG_USERNAME,
-    password: process.env.PG_PASSWORD
-}) : neon();
+    password: process.env.PG_PASSWORD});
+} else if (process.env.NODE_ENV==="production"){
+    sql = neon();
+}
