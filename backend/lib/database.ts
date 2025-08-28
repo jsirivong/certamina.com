aa75c0057a18c049f7963cef7e7e47d497e97d2a
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-28 15:50:51 -0400

    database connection changes

--- backend/lib/database.ts
+++ backend/lib/database.ts
@@ -4,15 +4,9 @@ import dotenv from 'dotenv'
 
 dotenv.config();
 
-export const sql = neon(process.env.NETLIFY_DATABASE_URL);
-
-// if (process.env.NODE_ENV === "development"){
-//     sql = postgres({
-//     host: process.env.PG_HOST,
-//     port: Number(process.env.PG_PORT) || 5432,
-//     database: process.env.PG_DATABASE,
-//     username: process.env.PG_USERNAME,
-//     password: process.env.PG_PASSWORD});
-// } else if (process.env.NODE_ENV==="production"){
-//     sql = neon();
-// }
+export const sql: any = process.env.NODE_ENV === "production" ? neon() : postgres({
+    host: process.env.PG_HOST,
+    port: Number(process.env.PG_PORT) || 5432,
+    database: process.env.PG_DATABASE,
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD});
