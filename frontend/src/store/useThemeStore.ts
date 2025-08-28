27f5228ed7b56fccbb5bb6c70231fcc9ac2b45e0
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-08-28 04:53:49 -0400

    custom theme store using zustand

--- frontend/src/store/useThemeStore.ts
+++ frontend/src/store/useThemeStore.ts
@@ -1,18 +1,18 @@
-// import { create } from 'zustand';
-// // "create" is a function, the function returns a custom hook
+import { create } from 'zustand';
+// "create" is a function, the function returns a custom hook
 
-// export type Theme = "light" | "dark";
+export type Theme = "light" | "dark";
 
-// interface IThemeStore {
-//     theme: Theme;
-//     setTheme: (theme: Theme) => void;
-// }
+interface IThemeStore {
+    theme: Theme;
+    setTheme: (theme: Theme) => void;
+}
 
-// // a custom hook
-// export const useThemeStore = create<IThemeStore>()((set) => ({
-//     theme: localStorage.getItem("theme") as Theme || "light",
-//     setTheme: (theme: Theme) => {
-//         set({ theme: theme });
-//         localStorage.setItem("theme", theme);
-//     }
-// }))
+// a custom hook
+export const useThemeStore = create<IThemeStore>()((set) => ({
+    theme: localStorage.getItem("theme") as Theme || "light",
+    setTheme: (theme: Theme) => {
+        set({ theme: theme });
+        localStorage.setItem("theme", theme);
+    }
+}))
