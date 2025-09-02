487d8e68399301fb03df915b184d3215b9c37d90
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-09-02 15:25:26 -0400

    socket configuration

--- frontend/src/sockets/socket.ts
+++ frontend/src/sockets/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const URL = import.meta.env.PROD ? "https://certamina.com" : "ws://localhost:3000";
+const URL = import.meta.env.PROD ? process.env.VITE_PROD_SOCKET_URL : process.env.VITE_DEV_SOCKET_URL;
 
 export const socket = io(URL)
