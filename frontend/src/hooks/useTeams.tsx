b3a711178741e929269e434a9f638cbd2b9eb8bc
pizza-eater-3500 <sirivongjonathan@gmail.com>
2025-09-02 15:09:20 -0400

    useteams custom hook

--- frontend/src/hooks/useTeams.tsx
+++ frontend/src/hooks/useTeams.tsx
@@ -1,39 +1,39 @@
-// import { useState } from "react"
+import { useState } from "react"
 
-// interface Player {
-//     id?: string;
-//     username: string;
-//     profile_picture?: string;
-// }
+interface Player {
+    id?: string;
+    username: string;
+    profile_picture?: string;
+}
 
-// interface Team {
-//     id: number;
-//     name: string;
-//     score: number;
-//     players: Player[];
-// }
+interface Team {
+    id: number;
+    name: string;
+    score: number;
+    players: Player[];
+}
 
-// export default function useTeams(){
-//     const [ teams, setTeams ] = useState<Team[]>([
-//         {
-//             id: 1,
-//             name: "Team 1",
-//             score: 0,
-//             players: []
-//         },
-//         {
-//             id: 2,
-//             name: "boomshakalaka",
-//             score: 0,
-//             players: []
-//         },
-//         {
-//             id: 3,
-//             name: "Team 3",
-//             score: 0,
-//             players: []
-//         }
-//     ]);
+export default function useTeams(){
+    const [ teams, setTeams ] = useState<Team[]>([
+        {
+            id: 1,
+            name: "Team 1",
+            score: 0,
+            players: []
+        },
+        {
+            id: 2,
+            name: "boomshakalaka",
+            score: 0,
+            players: []
+        },
+        {
+            id: 3,
+            name: "Team 3",
+            score: 0,
+            players: []
+        }
+    ]);
     
-//     return { teams, setTeams }
-// }
+    return { teams, setTeams }
+}
