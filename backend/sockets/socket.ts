1cd8651cabd33c3b25a6816c7dcac8d35befa560
pizza-eater-3500 <sirivongjonathan@gmail.com>
2026-09-01 22:06:18 -0400

    feat: allow rejoin room on disconnect and refresh

--- backend/sockets/socket.ts
+++ backend/sockets/socket.ts
@@ -4,11 +4,15 @@ import redis from '../services/redis.ts';
 
 import type { RoomData, Player, Team } from '../controllers/room.controller.ts';
 
+interface SocketData {
+    roomcode: string;
+    username: string;
+}
+
 export const initializeSocketIOServer = (io: Server) => {
     let timeoutLookups = new Map<string, NodeJS.Timeout>();
     // when a new socket connects
-    io.on("connection", (socket: Socket) => {
-        console.log("new websocket connected");
+    io.on("connection", async (socket: Socket) => {
         socket.on("create-room", (user, callback) => {
             createRoom(socket, user, callback);
         })
@@ -17,25 +21,41 @@ export const initializeSocketIOServer = (io: Server) => {
             joinRoom(socket, io, data, callback)
         })
 
+        socket.on("rejoin-room", async (code: string, username: string, callback: Function) => {
+            if (!code || !username) return;
+
+            if (timeoutLookups.has(username)) {
+                clearTimeout(timeoutLookups.get(username)!);
+                timeoutLookups.delete(username);
+
+                socket.join(`room:${code}`);
+                socket.data.roomcode = code;
+                socket.data.username = username;
+
+                const roomJSON = await redis.get(`room:${code}`);
+
+                if (!roomJSON) return;
+
+                const roomData: RoomData = JSON.parse(roomJSON);
+
+                roomData.teams.forEach((team: Team) => {
+                    team.players.forEach((player: Player) => {
+                        if (player.username === username) {
+                            player.socket_id = socket.id;
+                        }
+                    })
+                })
+
+                socket.emit("join-room", { room: roomData })
+            }
+        })
+
         socket.on("delete-room", (data) => {
             deleteRoom(socket, data);
         })
 
         socket.on("leave-room", (roomCode: string) => {
             leaveRoom(socket, io, roomCode)
-        })
-
-        socket.on("rejoin-room", async (username: string, roomCode: string) => {
-            console.log("Username: " + username);
-            if (timeoutLookups.has(username)) {
-                clearTimeout(timeoutLookups.get(username)!);
-                timeoutLookups.delete(username);
-            }
-
-            console.log("Here is the lookups: " + timeoutLookups);
-            socket.join(`room:${roomCode}`);
-            socket.data.roomcode = roomCode;
-            socket.data.username = username;
         })
 
         socket.on("chat-message", async (data) => {
