d559cd1b0c268c55d4c411abc381515520cb9ae2
pizza-eater-3500 <sirivongjonathan@gmail.com>
2026-09-01 22:32:20 -0400

    perf(socket): change autoconnect to true

--- frontend/src/services/socket.ts
+++ frontend/src/services/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_DEV_SOCKET_URL, { autoConnect: false });
+const socket = io(import.meta.env.VITE_DEV_SOCKET_URL, { autoConnect: true });
 
 export default socket;
