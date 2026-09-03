9ec5391eef9ba84ab733ac0c4053c052d6cf980d
pizza-eater-3500 <sirivongjonathan@gmail.com>
2026-09-03 02:53:26 -0400

    feat: add hover cursor pointer on created text

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -6,12 +6,12 @@ export default function Footer({ children }: IProps) {
     return (
         <div className="h-screen overflow-y-auto overflow-x-hidden">
             {children}
-            <footer className="footer gap-y-2 text-base-content/50 p-4 bg-base-200 flex justify-between items-center">
+            <footer className="footer gap-y-2 text-base-content/50 p-4 px-10 bg-base-200 flex justify-between items-center">
                 <nav className="flex flex-row items-center">
                     <a href="/privacy-policy">Privacy Policy</a> |
                     <a href="/terms-of-service">Terms of Service</a> |
                     <a>Cookie Settings</a> |
-                    <a>Created by Jonathan Sirivong</a>
+                    <a className="hover: cursor-pointer">Created by Jonathan Sirivong</a>
                 </nav>
 
                 <div className="flex flex-row items-center gap-x-4">
